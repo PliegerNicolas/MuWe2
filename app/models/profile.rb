7b6de8d04c61f5eef25abfec951d94a7ec5a96b0
Nicolas Plieger <plieger05@gmail.com>
2020-04-04 09:03:29 +0100

    Removing default image to profile model because it doesn't work

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,4 @@
 class Profile < ApplicationRecord
-  after_create :attach_default_profile_image
   after_validation :geocode
 
   reverse_geocoded_by :latitude, :longitude
@@ -8,13 +7,4 @@ class Profile < ApplicationRecord
   has_one :address
   has_many :posts
   has_one_attached :photo
-
-  def attach_default_profile_image
-    require 'open-uri'
-
-    image_path = 'https://extraupdate.com/wp-content/uploads/2019/02/map_img_1138084_1501023103.jpg'
-    file = URI.open(image_path)
-    filename = File.basename(URI.parse(image_path).path)
-    photo.attach(io: file, filename: filename)
-  end
 end
