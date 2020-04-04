7b0eb378b5d844584d6bdfa4eae96cd33bb37006
Nicolas Plieger <plieger05@gmail.com>
2020-04-04 08:26:54 +0100

    Index for not signed_in and first_name in sign_up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,51 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:index]
+  before_action :user_last_activity_timer
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, except: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # def user_not_authorized
+  #   flash[:alert] = "You are not authorized to perform this action."
+  #   redirect_to(root_path)
+  # end
+
+  private
+
+  def configure_permitted_parameters
+    # Adding first_name to devise form
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :email, :password)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :email, :password, :current_password)
+    end
+  end
+
+  def user_last_activity_timer
+    # Last user activity
+    return unless current_user
+
+    current_user.last_activity = DateTime.now
+    current_user.save
+  end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^errors$)/
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 end
