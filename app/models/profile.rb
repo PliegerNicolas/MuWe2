263dca959271b5d5ad1a86c4c4b1ef653035c9e0
Nicolas Plieger <plieger05@gmail.com>
2020-04-04 09:26:43 +0100

    Default image on user creating + profile creation working

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ApplicationRecord
+  after_create :attach_default_profile_photo
   after_validation :geocode
 
   reverse_geocoded_by :latitude, :longitude
@@ -7,4 +8,13 @@ class Profile < ApplicationRecord
   has_one :address
   has_many :posts
   has_one_attached :photo
+
+  def attach_default_profile_photo
+    require 'open-uri'
+
+    image_path = 'https://extraupdate.com/wp-content/uploads/2019/02/map_img_1138084_1501023103.jpg'
+    file = URI.open(image_path)
+    filename = File.basename(URI.parse(image_path).path)
+    self.photo.attach(io: file, filename: filename)
+  end
 end
