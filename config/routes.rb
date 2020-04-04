1327bc68eaac98536a845a0ccb6054376dfcd86b
Nicolas Plieger <plieger05@gmail.com>
2020-04-04 08:10:28 +0100

    Error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Error pages
+
+  get '404', to: 'errors#not_found'
+  get '422', to: 'errors#unacceptable'
+  get '500', to: 'errors#internal_error'
 end
