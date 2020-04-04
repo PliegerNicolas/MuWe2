586dd836ddb48876843759a881c8e383384c90b3
Nicolas Plieger <plieger05@gmail.com>
2020-04-04 07:58:13 +0100

    Correcting votes migration

--- db/migrate/20200404061332_create_votes.rb
+++ db/migrate/20200404061332_create_votes.rb
@@ -2,8 +2,8 @@ class CreateVotes < ActiveRecord::Migration[6.0]
   def change
     create_table :votes do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
-      t.references :comment, null: false, foreign_key: true
+      t.references :post, null: true, foreign_key: true
+      t.references :comment, null: true, foreign_key: true
       t.integer :rating, default: 0
 
       t.timestamps
