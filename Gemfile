36f6436251d41b09bc1a21d66436b7fbd25d8b34
Nicolas Plieger <plieger05@gmail.com>
2020-04-04 08:41:12 +0100

    Geocoding on profile model and default image attachment

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'redis'
 
 gem 'cloudinary'
 gem 'pundit'
+gem 'geocoder'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,20 @@
 class Profile < ApplicationRecord
+  after_create :attach_default_profile_image
+  after_validation :geocode
+
+  reverse_geocoded_by :latitude, :longitude
+
   belongs_to :user
   has_one :address
   has_many :posts
   has_one_attached :photo
+
+  private
+
+  def attach_default_profile_image
+    image_path = 'https://extraupdate.com/wp-content/uploads/2019/02/map_img_1138084_1501023103.jpg'
+    file = URI.open(image_path)
+    filename = File.basename(URI.parse(image_path).path)
+    photo.attach(io: file, filename: filename)
+  end
 end
