f914899695ee4ca4db363739b6a7efb2d76c3f34
Nicolas Plieger <plieger05@gmail.com>
2020-04-05 14:15:14 +0100

    Removing notice because it should work with cookies

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'jams#index'
 
-  # Permissions for saving User location
-
-  get 'accept_location_save', to: 'userlocations#permit_location_save', as: :permit_location_save
-  get 'deny_location_save', to: 'userlocations#deny_location_save', as: :deny_location_save
-
   # Error pages
 
   get '404', to: 'errors#not_found'
