326d6b4187da3ce49d20399739b655712b6b631e
Nicolas Plieger <plieger05@gmail.com>
2020-04-05 16:27:13 +0100

    Private methods for creating cookie and deleting cookie in profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,14 @@
 class ProfilesController < ApplicationController
+
+  private
+
+  def set_privacy_cookie
+    # Set cookie user_privacy_status to string true
+    cookies.permanent.signed[:user_privacy_status] = "true"
+  end
+
+  def delete_privacy_cookie
+    # Delete cookie user_privacy_status a.k.a set it to false
+    cookies.delete :user_privacy_status
+  end
 end
