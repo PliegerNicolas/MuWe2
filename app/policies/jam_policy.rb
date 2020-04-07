99ca8d2594001c330eea32e8a79c0723bcdbe138
Nicolas Plieger <plieger05@gmail.com>
2020-04-07 11:37:48 +0100

    Setting up policies for JAM

--- app/policies/jam_policy.rb
+++ app/policies/jam_policy.rb
@@ -8,4 +8,20 @@ class JamPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def show?
+    true
+  end
+
+  def create?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
