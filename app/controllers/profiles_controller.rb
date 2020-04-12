83a9a5bf1fd31f38ed1f843b6b8f57eb8331b1df
Nicolas Plieger <plieger05@gmail.com>
2020-04-12 18:03:31 +0100

    Routes to personnal profile page and public profile page setup

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,11 @@
 class ProfilesController < ApplicationController
+  def my_profile
+    @profile = current_user.profile
+    authorize @profile
+  end
+
+  def public_profile
+    @profile = Profile.find(params[:id])
+    authorize @profile
+  end
 end
