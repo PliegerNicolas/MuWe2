4ba1c340008790b2b41382d3c5efc2928d2b7396
Nicolas Plieger <plieger05@gmail.com>
2020-04-12 18:04:36 +0100

    Better route name

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Profile routes
 
   get 'my_profile', to: 'profiles#my_profile', as: :my_profile
-  get 'profile/:id', to: 'profiles#public_profile', as: :public_profile
+  get 'profile/:id', to: 'profiles#public_profile', as: :profile
 
   # User privacy settings
 
