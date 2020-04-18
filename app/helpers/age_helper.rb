48d267f3135d3d6a1be3579539aa6ad7bf242009
Nicolas Plieger <plieger05@gmail.com>
2020-04-18 17:17:19 +0100

    Better age helper

--- app/helpers/age_helper.rb
+++ app/helpers/age_helper.rb
@@ -1,5 +1,5 @@
 module AgeHelper
   def age(birth_date)
-    ((Time.zone.now – birth_date.to_time) / 1.year.seconds).floor if birth_date
+    ((Time.zone.now - birth_date.to_time) / 1.year.seconds).floor.to_s + " years old" if birth_date
   end
 end
